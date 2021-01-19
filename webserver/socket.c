ea5b3de7110f62b7c9a67127b5d92c2c9beeeec2
guilamb <guilhanebourgin@gmail.com>
2021-01-19 18:15:04 +0100

    03-REUSEADDR

--- webserver/socket.c
+++ webserver/socket.c
@@ -22,11 +22,11 @@ int creer_serveur(int port, int option){
     saddr.sin_family = AF_INET; /* Socket ipv4 */
     saddr.sin_port = htons(port); /* Port d'écoute */
     saddr.sin_addr.s_addr = INADDR_ANY; /* écoute sur toutes les interfaces */
-/*
+
     int optval = 1;
     if(setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval,sizeof(int)) == -1)
         perror("Can not set SO_REUSEADDR option");
-*/
+
     if (bind(socket_serveur, (struct sockaddr *)&saddr, sizeof(saddr)) == -1) {
         perror("bind socker_serveur");
         return -1;
